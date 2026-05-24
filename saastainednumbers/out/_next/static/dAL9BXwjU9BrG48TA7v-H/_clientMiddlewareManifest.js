6a91f2779cace4f67d6a5327f8832f6573fde5c1
ng258srt8p-ship-it <ng258srt8p@privaterelay.appleid.com>
2026-05-24 19:10:25 -0400

    Reorganize project: move source to root, build output to out/

--- saastainednumbers/out/_next/static/dAL9BXwjU9BrG48TA7v-H/_clientMiddlewareManifest.js
+++
@@ -1,6 +0,0 @@
-self.__MIDDLEWARE_MATCHERS = [
-  {
-    "regexp": "^(?:\\/(_next\\/data\\/[^/]{1,}))?(?:\\/((?!api|_next|.*\\..*).*))(\\.json|\\.rsc|\\.segments\\/.+\\.segment\\.rsc)?[\\/#\\?]?$",
-    "originalSource": "/((?!api|_next|.*\\..*).*)"
-  }
-];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
