3ee8c84dbd80e5f1ed41a80652848d065d06be33
ng258srt8p-ship-it <ng258srt8p-ship-it@users.noreply.github.com>
2026-06-20 15:18:01 -0400

    fix: match canvas container bg to sidebar, add padding-top to clear nav

--- app/canvas/page.tsx
+++ app/canvas/page.tsx
@@ -83,7 +83,7 @@ function CanvasPageInner() {
   }, []);
 
   return (
-    <div className="flex h-[calc(100vh-64px)]" style={{ height: "calc(100vh - 64px)" }}>
+    <div className="flex h-screen bg-white dark:bg-gray-800 overflow-hidden" style={{ height: "100vh", paddingTop: "66px", boxSizing: "border-box" }}>
       {/* Catalog sidebar */}
       <CalculatorCatalog
         onAddCalculator={addCalculator}
